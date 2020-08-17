3effd201700918fdf9370f8e1ad6e49b5a4e939f
Takanori <62200482+Taka0119@users.noreply.github.com>
2020-08-17 22:59:48 +0900

    deviseの設定を変更
    deviseで作成されたviewは、viewファイルを編集するだけでは反映されない為、config.scoped_views = trueを追記

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -244,7 +244,8 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  
+  config.scoped_views = true # 左記の記述でdevisで作成されたviewの編集が反映される。追記後は、bundle installを行い、サーバーを再起動。
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
