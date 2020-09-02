c92aca9203986992bb7b14fec287f34afe62e8a6
Takanori <62200482+Taka0119@users.noreply.github.com>
2020-09-02 23:55:39 +0900

    プルダウンメニュー実装(未完)

--- app/javascript/modules/header.js
+++ app/javascript/modules/header.js
@@ -1,16 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
+  function pulldown() {
+    let menu = document.getElementsByClassName('shares_header__box__menu__movie');
 
-  function textAdd() {
-    let block = document.getElementById('menu_movie')
-    insetElement = document.createElement('li');
-
-    insetElement.textContent = 'TEST :';
-    block.insertAdjacentElement('afterbegin', insetElement)
   }
 
-  document.getElementById('menu_movie').addEventListener('click', textAdd)
-
-  let window = document.defaultView
-  let header = document.getElementsByClassName('shares_header')[0]
-
+  document.getElementsByClassName('shares_header__box__menu__movie')
 });
+
